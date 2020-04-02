5ae25cd6ce34d0970c9c0580a1c94fcac74fcb1f
Vladimir Troflianin <vladimir.troflianin@it.adcombo.com>
2020-04-02 12:26:32 +0300

    Add compatibility with http-errors to jsonapi_exception_formatter

--- flask_rest_jsonapi/errors.py
+++ flask_rest_jsonapi/errors.py
@@ -1,4 +1,12 @@
 """Helper to  format Api errors according to jsonapi specification"""
+from flask_rest_jsonapi.exceptions import BadRequest, ObjectNotFound, InvalidType, AccessDenied
+
+STATUS_MAP = {
+    400: BadRequest,
+    404: ObjectNotFound,
+    409: InvalidType,
+    403: AccessDenied,
+}
 
 
 def jsonapi_errors(jsonapi_errors):
@@ -9,3 +17,23 @@ def jsonapi_errors(jsonapi_errors):
     """
     return {'errors': [jsonapi_error for jsonapi_error in jsonapi_errors],
             'jsonapi': {'version': '1.0'}}
+
+
+def format_http_exception(ex):
+    """
+    try to format http exception to jsonapi 1.0
+    Warning! It only works for errors with status code less than 500
+    :param ex: http exception
+    :return:
+    """
+    code = getattr(ex, 'code', None)
+    try:
+        status = int(code)
+    except TypeError:
+        return
+
+    api_ex = STATUS_MAP.get(status)
+    if not api_ex:
+        return
+
+    return api_ex(detail=getattr(ex, 'description', ''))
