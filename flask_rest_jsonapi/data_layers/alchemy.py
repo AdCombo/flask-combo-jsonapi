46df5de0d13c814aff84f02405dacc88837239cc
Suren Khorenyan <khorenyan@it.adcombo.com>
2020-04-21 01:12:00 +0300

    Set disable_collection_count and default_collection_count in alchemy's post_init

--- flask_rest_jsonapi/data_layers/alchemy.py
+++ flask_rest_jsonapi/data_layers/alchemy.py
@@ -50,6 +50,37 @@ class SqlalchemyDataLayer(BaseDataLayer):
                 f"You must provide a model in data_layer_kwargs to use sqlalchemy data layer in {self.resource.__name__}"
             )
 
+        self.disable_collection_count: bool = False
+        self.default_collection_count: int = -1
+
+    def post_init(self):
+        """
+        Checking some props here
+        :return:
+        """
+        if self.resource is None:
+            # if working outside the resource, it's not assigned here
+            return
+
+        if not hasattr(self.resource, "disable_collection_count") or self.resource.disable_collection_count is False:
+            return
+
+        params = self.resource.disable_collection_count
+
+        if isinstance(params, (bool, int)):
+            self.disable_collection_count = bool(params)
+
+        if isinstance(params, (tuple, list)):
+            try:
+                self.disable_collection_count, self.default_collection_count = params
+            except ValueError:
+                raise ValueError(
+                    "Resource's attribute `disable_collection_count` "
+                    "has to be bool or list/tuple with exactly 2 values!\n"
+                    "For example `disable_collection_count = (True, 999)`"
+                )
+        # just ignoring other types, we don't know how to process them
+
     def create_object(self, data, view_kwargs):
         """Create an object through sqlalchemy
 
@@ -153,6 +184,9 @@ class SqlalchemyDataLayer(BaseDataLayer):
         :param view_kwargs: view kwargs
         :return:
         """
+        if self.disable_collection_count is True:
+            return self.default_collection_count
+
         return query.count()
 
     def get_collection(self, qs, view_kwargs):
