1ca12a62869936b807a54651d056a2ab3968b220
Suren Khorenyan <surenkhorenyan@gmail.com>
2021-09-01 21:24:21 +0300

    bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-__version__ = "1.0.7"
+__version__ = "1.1.0"
 
 
 requirements_filepath = os.path.join(os.path.dirname(__name__), "requirements.txt")
