ad22482c58028efd182eecc6d5f5d67265600619
Paul Warren <pdw@ex-parrot.com>
2023-08-03 10:08:39 +0100

    Restrict "except" to expected ValueError exception.

--- flask_combo_jsonapi/decorators.py
+++ flask_combo_jsonapi/decorators.py
@@ -73,9 +73,11 @@ def jsonapi_exception_formatter(func):
         try:
             return func(*args, **kwargs)
         except JsonApiException as e:
+            # If status is just a numeric code, convert it to an int so that
+            # flask expands it to a valid HTTP status line in the response.
             try:
                 status = int(e.status)
-            except:
+            except ValueError:
                 status = e.status
             return make_response(jsonify(jsonapi_errors([e.to_dict()])),
                                  status,
